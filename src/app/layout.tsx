9e90c4e18613d0e99fa89c17b635f1c0b7494557
gitpancake <pye.henry@protonmail.com>
2023-07-14 13:31:10 -0400

    More metatag updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,13 +18,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta property="og:title" content="Winneres Draw" />
         <meta property="og:site_name" content="Winneres Draw" />
         <meta property="og:description" content="A website with an Ethereum blockchain connection to read winners from the results contract." />
-        <meta property="og:image" content="/placeholder.png" />
-        <meta property="og:image:secure_url" content="/placeholder.png" />
+        <meta property="og:image" content="https://winners.henrypye.xyz/placeholder.png" />
+        <meta property="og:image:secure_url" content="https://winners.henrypye.xyz/placeholder.png" />
         <meta property="og:url" content={`https://winners.henrypye.xyz`} />
         <meta property="og:image:width" content="708" />
         <meta property="og:image:height" content="372" />
         <meta name="twitter:title" content="Winneres Draw" />
-        <meta name="twitter:image" content="/placeholder.png" />
+        <meta name="twitter:image" content="https://winners.henrypye.xyz/placeholder.png" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:description" content="A website with an Ethereum blockchain connection to read winners from the results contract." />
         <meta name="twitter:url" content={`https://winners.henrypye.xyz`} />
